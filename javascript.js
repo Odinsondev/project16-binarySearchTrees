6c8823397ded86a1f3d37ed30ed32dc0d65bb750
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-11-15 23:26:57 -0800

    Feat: add functions: node, removeDuplicateValues

---
+++ javascript.js
@@ -0,0 +1,41 @@
+//Balanced Binary Search Tree
+
+function node(data) {
+  const node = {};
+  node.data = data;
+  node.left = null;
+  node.right = null;
+
+  return node;
+}
+
+function tree(array) {
+  const balancedBST = {};
+  balancedBST.root = '';
+}
+
+function removeDuplicateValues(array) {
+  let newArray = array;
+
+  //iterates through the array once
+  for (let i = 0; i < newArray.length; i++) {
+    let elementFound = false;
+
+    //iterates through the array during each iteration
+    //comparing each value to each other value
+    for (let j = 0; j < newArray.length; j++) {
+      //if first time comparing a value to the same value
+      if (newArray[i] === newArray[j] && elementFound === false) {
+        elementFound = true;
+        //if comparing a value to a duplicate value
+      } else if (newArray[i] === newArray[j] && elementFound === true) {
+        newArray.splice(j, 1);
+      }
+    }
+  }
+  return newArray;
+}
+
+let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+console.log(removeDuplicateValues(testArray));
