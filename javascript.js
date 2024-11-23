40c30dd3722af2e1263fffdb00ebcb87a6c2da39
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-11-22 22:15:55 -0800

    Feat: Create tree factory function

--- javascript.js
+++ javascript.js
@@ -12,6 +12,35 @@ function node(data) {
 function tree(array) {
   const balancedBST = {};
   balancedBST.root = '';
+  balancedBST.sortedArray = sortNumericArray(removeDuplicateValues(array));
+
+  balancedBST.buildTree = function () {
+    return sortedArrayToBSTRecur(this.sortedArray, 0, array.length - 1);
+
+    function sortedArrayToBSTRecur(array, start, end) {
+      //Termination condition for the recursive function
+      if (start > end) return null;
+
+      //Find the middle element
+      let mid = Math.floor((start + end) / 2);
+
+      //Create root node
+      let root = node(array[mid]);
+
+      //Create left subtree
+      root.left = sortedArrayToBSTRecur(array, start, mid - 1);
+
+      //Create right subtree
+      root.right = sortedArrayToBSTRecur(array, mid + 1, end);
+
+      return root;
+    }
+  };
+
+  //Can only use the function after it has been defined
+  balancedBST.root = balancedBST.buildTree();
+
+  return balancedBST;
 }
 
 function removeDuplicateValues(array) {
@@ -43,6 +72,22 @@ function sortNumericArray(array) {
   return array;
 }
 
+//TestArray and testTree
 let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const testTree = tree(testArray);
 
-console.log(sortNumericArray(removeDuplicateValues(testArray)));
+//Function provided by The Odin Project to console.log the tree
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+console.log(prettyPrint(testTree.root));
